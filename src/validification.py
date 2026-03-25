68a23c47f2d47007714c699ef07530c4a4f930c4
DigitalRage <blaine.buckler@ucas-edu.net>
2026-03-25 09:20:45 -0600

    Fix

--- src/validification.py
+++
@@ -1,7 +0,0 @@
-FUNCTION get_valid_input(prompt):
-    LOOP:
-        TRY:
-            user_input = int(input(prompt))
-            RETURN user_input
-        EXCEPT:
-            DISPLAY "Invalid input. Please enter a number."
